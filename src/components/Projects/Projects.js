d39b4fb6d6e7717f6ae45dec9b43c70429d48dab
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-10-07 09:50:43 +0300

    Add Grid and Flexbox Layout

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,27 @@
 import "./Projects.css";
 import Project from "../Project/Project";
+import { GrPrevious, GrNext } from "react-icons/gr"
+import { useState } from "react";
 
 function Projects( props ) {
+  const [pageNum, setPageNum] = useState(1);
 
-  let projects = props.projectsInfo.map( (item, index) => {
+  let totalPage = Math.ceil(props.projectsInfo.length / 6);
+
+  const increment = () => {
+    if(pageNum !== totalPage) {
+      setPageNum(pageNum + 1);
+    }
+  }
+  const decrement = () => {
+    if(pageNum !== 1) {
+      setPageNum(pageNum - 1);
+    }
+  }
+
+  let projectsCurrent = props.projectsInfo.slice((pageNum - 1) * 6, (pageNum - 1) * 6 + 6 )
+
+  let projects = projectsCurrent.map( (item, index) => {
     return (
       <Project
         key={index}
@@ -19,8 +37,14 @@ function Projects( props ) {
 
   return (
     <div className="projects-container">
-      <h2 className="projects-title">Projects ({props.projectsInfo.length})</h2>
+      <div className="project-header">
+        <h2 className="projects-title">Projects ({props.projectsInfo.length})</h2>
+        <button onClick={decrement} className="project-btn"><GrPrevious /></button> Page {pageNum} of {totalPage}<button onClick={increment} className="project-btn"><GrNext /></button>
+      </div>
       { projects }
+      <div className="page-nav">
+        <button onClick={decrement} className="project-btn"><GrPrevious /></button> Page {pageNum} of {totalPage}<button onClick={increment} className="project-btn"><GrNext /></button>
+      </div>
     </div>
   );
 }
